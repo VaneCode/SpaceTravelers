a2cb4fb66eea474024dc046d53945b4fb8e6b618
vanessaBonito <vanebonito@hotmail.com>
2022-09-01 14:30:26 -0500

    Add styles to Navigation component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,47 +1,75 @@
-import { NavLink, Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import planet from '../images/planet.png';
+import './Navbar.css';
 
-const Navigation = () => (
-  <Navbar
-    bg="light"
-    expand="lg"
-    style={{ borderBottomStyle: '1rem solid #888' }}
-  >
-    <Container className="d-flex flex-row justify-content-around aling-content-center" style={{ margin: '0', widt: '100%' }}>
+const Navigation = () => {
+  const [state, setState] = useState(1);
+  return (
+    <Navbar
+      bg="light"
+      expand="lg"
+      style={{
+        borderBottom: '0.01rem solid #888',
+        width: '100%',
+        padding: '0.5rem',
+      }}
+    >
       <div>
-        <img src={planet} alt="logo" style={{ marginRight: '1rem' }} />
+        <img
+          src={planet}
+          alt="logo"
+          style={{ marginRight: '0.5rem', marginLeft: '1rem' }}
+        />
       </div>
       <Navbar.Brand href="/" className="mr-auto p-2">
-        <Link to="/">Space Traveler&apos;s Hub</Link>
+        <Link to="/" className="brand">
+          Space Traveler&apos;s Hub
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="Navbar toggle" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto justify-self-end">
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? 'rockets-link active-link' : 'none')}
-          >
-            Rockets
-          </NavLink>
-          <NavLink
-            to="/missions"
-            className={({ isActive }) => (isActive ? 'missions-link active-link' : 'none')}
-          >
-            Missions
-          </NavLink>
-          <NavLink
-            to="/profile"
-            className={({ isActive }) => (isActive ? 'myprofile-link active-link' : 'none myprofile-none')}
-          >
-            My profile
-          </NavLink>
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav>
+          <Nav.Link>
+            <Link
+              to="/"
+              style={{
+                textDecoration: state === 1 ? 'underline' : 'none',
+              }}
+              onClick={() => setState(1)}
+            >
+              Rockects
+            </Link>
+          </Nav.Link>
+          <Nav.Link className="nonActive">
+            <Link
+              to="/missions"
+              style={{
+                textDecoration: state === 2 ? 'underline' : 'none',
+              }}
+              onClick={() => setState(2)}
+            >
+              Missions
+            </Link>
+          </Nav.Link>
+          <div className="line" />
+          <Nav.Link className="nonActive">
+            <Link
+              to="/profile"
+              style={{
+                textDecoration: state === 3 ? 'underline' : 'none',
+              }}
+              onClick={() => setState(3)}
+            >
+              My profile
+            </Link>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+    </Navbar>
+  );
+};
 
 export default Navigation;
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,14 +1,12 @@
-import ReservedRockets from "../../components/ReservedRockets/ReservedRockets";
-import "bootstrap/dist/css/bootstrap.min.css";
+import ReservedRockets from '../../components/ReservedRockets/ReservedRockets';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Profile = () => {
-  return (
-    <div className="container-fluid" style={{ padding: "2rem" }}>
-      <div className="d-flex">
-        <ReservedRockets />
-      </div>
+const Profile = () => (
+  <div className="container-fluid" style={{ padding: '2rem' }}>
+    <div className="d-flex">
+      <ReservedRockets />
     </div>
-  );
-};
+  </div>
+);
 
 export default Profile;
