baefe8c6d81bf0539187c8bbec6c0ec9a9ee09dd
Vanessa Bonito <vanebonito@hotmail.com>
2022-09-02 18:26:38 -0500

    Update Mission.js to fix eslinter error

--- src/components/Mission/Mission.js
+++ src/components/Mission/Mission.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-props-no-multi-spaces */
+/* eslint-disable react/react/no-unknown-property */
 import { useSelector, useDispatch } from 'react-redux';
 import React, { useEffect } from 'react';
 import { getMissionData, joinMission } from '../../redux/missions/missions';
